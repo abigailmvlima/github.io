319132a9ac7270a5507dae0643d299780b044086
elio.lima <macstudio@local>
2023-03-31 14:41:05 -0300

    feat(Fix): ajust coments

--- js/services.js
+++ js/services.js
@@ -3,7 +3,6 @@ $.fn.searchData = async function () {
     const url = "https://api.content.tripadvisor.com/api/v1/location/search?key=A0A244D2D5D8441CBE2DEFCCF1958378&searchQuery=hotel&category=hotels&latLong=-23.537430585909647%252C%2520-46.51669981349255&language=pt-br";
     const options = {
         method: 'GET',
-        headers: { accept: 'application/json' },
         mode: 'no-cors',
     };
 
