d6fe5f38a86575ad100426d0b9a157261986b151
elio.lima <macstudio@local>
2023-03-31 19:08:09 -0300

    feat(Fix): ajust coments

--- js/services.js
+++ js/services.js
@@ -1,6 +1,6 @@
 
 $.fn.searchData = async function () {
-    const url = "https://api.content.tripadvisor.com/api/v1/location/search?key=A0A244D2D5D8441CBE2DEFCCF1958378&searchQuery=hotel&category=hotels&latLong=-23.537430585909647%252C%2520-46.51669981349255&language=pt-br";
+    const url = "https://p7167519uh.execute-api.us-east-1.amazonaws.com/dev/search?searchQuery=hotel&category=hotels&latLong=-23.537430585909647%252C%2520-46.51669981349255&language=pt-br";
     const options = {
         method: 'GET',
         mode: 'no-cors',
