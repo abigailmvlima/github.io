a49da3a18619fc1ece8a1f664b9c77b04c13d240
elio.lima <macstudio@local>
2023-03-31 14:28:09 -0300

    feat(Fix): ajust coments

--- js/services.js
+++ js/services.js
@@ -1,5 +1,5 @@
 
-$.fn.searchData = function () {
+$.fn.searchData = async function () {
     const url = "https://api.content.tripadvisor.com/api/v1/location/search?key=A0A244D2D5D8441CBE2DEFCCF1958378&searchQuery=hotel&category=hotels&latLong=-23.537430585909647%252C%2520-46.51669981349255&language=pt-br";
     const options = { 
         method: 'GET', 
@@ -7,10 +7,6 @@ $.fn.searchData = function () {
         mode: 'no-cors', 
     };
 
-    fetch(url, options)
-        .then(response => response )
-        .then(response => console.log(1111,response.json()) )
-        .catch(err => console.error(222, err));
-
-        
+    const response = await fetch(url, options)
+    console.log(1213, response)
 }
