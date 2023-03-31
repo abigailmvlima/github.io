ec349d48cda3c1532b00f8f75efdbd62120b5a75
elio.lima <macstudio@local>
2023-03-31 13:44:07 -0300

    feat(Fix): ajust coments

--- js/services.js
+++ js/services.js
@@ -1,21 +1,23 @@
 
 $.fn.searchData = async function () {
-    const url = "https://api.content.tripadvisor.com/api/v1/location/search?key=A0A244D2D5D8441CBE2DEFCCF1958378&searchQuery=hotel&category=hotels&latLong=-23.537430585909647%252C%2520-46.51669981349255&language=pt-br";
-    $.ajax({
-        url,
-        type: "GET",
-        crossDomain: false,
-        dataType: 'json',
-        headers: {            
-            "Host": "api.content.tripadvisor.com",
-            "Accept": '*',
-            'Access-Control-Allow-Origin': '*'
-        },
-        success: function (data) {
-            console.log("Resposta do servidor: ", data);
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.log(111, jqXHR, textStatus, errorThrown);
-        }
-    });
+    const url = "http://api.content.tripadvisor.com/api/v1/location/search?key=A0A244D2D5D8441CBE2DEFCCF1958378&searchQuery=hotel&category=hotels&latLong=-23.537430585909647%252C%2520-46.51669981349255&language=pt-br";
+    var jqxhr = $.get( url, function() {
+        console.log( "success" );
+      })
+        .done(function() {
+          console.log( "second success" );
+        })
+        .fail(function(data, a, b) {
+          console.log( "error", data, a, b );
+        })
+        .always(function() {
+          console.log( "finished" );
+        });
+       
+      // Perform other work here ...
+       
+      // Set another completion function for the request above
+      jqxhr.always(function() {
+        console.log( "second finished" );
+      });
 }
