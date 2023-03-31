0e6db6faa7122dbc6ead60950aa2c8db916d5b8f
elio.lima <macstudio@local>
2023-03-31 13:45:08 -0300

    feat(Fix): ajust coments

--- js/services.js
+++ js/services.js
@@ -1,6 +1,6 @@
 
 $.fn.searchData = async function () {
-    const url = "http://api.content.tripadvisor.com/api/v1/location/search?key=A0A244D2D5D8441CBE2DEFCCF1958378&searchQuery=hotel&category=hotels&latLong=-23.537430585909647%252C%2520-46.51669981349255&language=pt-br";
+    const url = "https://api.content.tripadvisor.com/api/v1/location/search?key=A0A244D2D5D8441CBE2DEFCCF1958378&searchQuery=hotel&category=hotels&latLong=-23.537430585909647%252C%2520-46.51669981349255&language=pt-br";
     var jqxhr = $.get( url, function() {
         console.log( "success" );
       })
