93f14c32923246d2cbc1610801275fa3047d472b
elio.lima <macstudio@local>
2023-03-31 13:57:19 -0300

    feat(Fix): ajust coments

--- js/services.js
+++ js/services.js
@@ -1,10 +1,22 @@
 
 $.fn.searchData = async function () {
     const url = "https://api.content.tripadvisor.com/api/v1/location/search?key=A0A244D2D5D8441CBE2DEFCCF1958378&searchQuery=hotel&category=hotels&latLong=-23.537430585909647%252C%2520-46.51669981349255&language=pt-br";
-    const options = { method: 'GET', headers: { accept: 'application/json' } };
 
-    fetch(url, options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+    var settings = {
+        'cache': false,
+        'dataType': "jsonp",
+        "async": true,
+        "crossDomain": true,
+        url,
+        "method": "GET",
+        "headers": {
+            "accept": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        }
+    }
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+
+    });
 }
