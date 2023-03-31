78b2e2f82321c0c907f533b7de0e8808778a588d
elio.lima <macstudio@local>
2023-03-31 19:39:14 -0300

    feat(Fix): ajust coments

--- js/services.js
+++ js/services.js
@@ -1,6 +1,7 @@
+$.url_service = "https://p7167519uh.execute-api.us-east-1.amazonaws.com/dev";
 
 $.fn.searchData = function () {
-    const url = "https://p7167519uh.execute-api.us-east-1.amazonaws.com/dev/search?searchQuery=hotel&category=hotels&latLong=-23.537430585909647%252C%2520-46.51669981349255&language=pt-br";
+    const url = `${$.url_service}/search?searchQuery=hotel&category=hotels&latLong=-23.537430585909647%252C%2520-46.51669981349255&language=pt-br`;
     fetch(url, {
         method: 'GET',
         withCredentials: true,
