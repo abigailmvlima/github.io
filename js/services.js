67062b04f43a0cd48223789e545eb67d77de777e
elio.lima <macstudio@local>
2023-03-31 13:47:59 -0300

    feat(Fix): ajust coments

--- js/services.js
+++ js/services.js
@@ -1,23 +1,10 @@
 
 $.fn.searchData = async function () {
     const url = "https://api.content.tripadvisor.com/api/v1/location/search?key=A0A244D2D5D8441CBE2DEFCCF1958378&searchQuery=hotel&category=hotels&latLong=-23.537430585909647%252C%2520-46.51669981349255&language=pt-br";
-    var jqxhr = $.get( url, function() {
-        console.log( "success" );
-      })
-        .done(function() {
-          console.log( "second success" );
-        })
-        .fail(function(data, a, b) {
-          console.log( "error", data, a, b );
-        })
-        .always(function() {
-          console.log( "finished" );
-        });
-       
-      // Perform other work here ...
-       
-      // Set another completion function for the request above
-      jqxhr.always(function() {
-        console.log( "second finished" );
-      });
+    const options = { method: 'GET', headers: { accept: 'application/json' } };
+
+    fetch(url, options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
 }
