d45bbed06c09344d4b6d16060afc4ed0a37b47bb
elio.lima <macstudio@local>
2023-03-31 13:18:46 -0300

    feat(Fix): ajust coments

--- js/load.js
+++ js/load.js
@@ -2,4 +2,6 @@ $( document ).ready(function() {
     var today = moment().format('YYYY-MM-DD');
     document.getElementById("dateStart").value = today;
     document.getElementById("dateEnd").value = today;
+
+    $.fn.searchData()
 });
---
+++ js/services.js
@@ -0,0 +1,21 @@
+
+$.fn.searchData = async function () {
+    const url = "https://api.content.tripadvisor.com/api/v1/location/search?key=A0A244D2D5D8441CBE2DEFCCF1958378&searchQuery=hotel&category=hotels&latLong=-23.537430585909647%252C%2520-46.51669981349255&language=pt-br";
+    $.ajax({
+        url,
+        type: "GET",
+        crossDomain: false,
+        dataType: 'json',
+        headers: {            
+            "Host": "api.content.tripadvisor.com",
+            "Accept": "*/*",
+            'Access-Control-Allow-Origin': '*'
+        },
+        success: function (data) {
+            console.log("Resposta do servidor: ", data);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(111, jqXHR, textStatus, errorThrown);
+        }
+    });
+}
