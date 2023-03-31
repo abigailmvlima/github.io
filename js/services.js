a0ac5c80ba420da0d5161abb14d3410c3b004775
elio.lima <macstudio@local>
2023-03-31 19:24:33 -0300

    feat(Fix): ajust coments

--- js/services.js
+++ js/services.js
@@ -1,20 +1,18 @@
 
 $.fn.searchData = async function () {
     const url = "https://p7167519uh.execute-api.us-east-1.amazonaws.com/dev/search?searchQuery=hotel&category=hotels&latLong=-23.537430585909647%252C%2520-46.51669981349255&language=pt-br";
-    const options = {
-        method: 'GET',
-        mode: 'no-cors',
-    };
-
-    try {
-        const response = await fetch(url, options);
-        if (!response.ok) {
-            console.log(4444, response)
-            throw new Error('Erro na solicitação.');
-        }
-        const data = await response.json();
-        console.log('Resposta do servidor: ', data);
-    } catch (error) {
-        console.error('Erro na solicitação: ', error);
-    }
+    fetch(url, {    
+      method: 'GET',    
+      withCredentials: true,    
+      crossorigin: true,    
+      mode: 'no-cors',       
+    })    
+      .then((res) => res.json())    
+      .then((data) => {    
+        console.log(data);    
+      })    
+      .catch((error) => {    
+        console.error(error);    
+      });    
+  };    
 }
