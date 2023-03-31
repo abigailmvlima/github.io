69b4159bf2d212bda1b8f5391bbe7009b9873469
elio.lima <macstudio@local>
2023-03-31 13:23:03 -0300

    feat(Fix): ajust coments

--- js/services.js
+++ js/services.js
@@ -1,21 +1,22 @@
 
 $.fn.searchData = async function () {
+    const xhr = new XMLHttpRequest();
     const url = "https://api.content.tripadvisor.com/api/v1/location/search?key=A0A244D2D5D8441CBE2DEFCCF1958378&searchQuery=hotel&category=hotels&latLong=-23.537430585909647%252C%2520-46.51669981349255&language=pt-br";
-    $.ajax({
-        url,
-        type: "GET",
-        crossDomain: false,
-        dataType: 'json',
-        headers: {            
-            "Host": "api.content.tripadvisor.com",
-            "Accept": "*/*",
-            'Access-Control-Allow-Origin': '*'
-        },
-        success: function (data) {
+
+    xhr.open('GET', url, true);
+
+    xhr.onload = function () {
+        if (this.status === 200) {
+            const data = JSON.parse(this.responseText);
             console.log("Resposta do servidor: ", data);
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.log(111, jqXHR, textStatus, errorThrown);
+        } else {
+            console.error("Erro na solicitação: ", this.statusText);
         }
-    });
+    };
+
+    xhr.onerror = function () {
+        console.error("Erro na solicitação.");
+    };
+
+    xhr.send();
 }
