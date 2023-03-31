819ca5a19fbace6da659d5f723226708b74c56a7
elio.lima <macstudio@local>
2023-03-31 14:38:34 -0300

    feat(Fix): ajust coments

--- js/services.js
+++ js/services.js
@@ -1,12 +1,20 @@
 
 $.fn.searchData = async function () {
     const url = "https://api.content.tripadvisor.com/api/v1/location/search?key=A0A244D2D5D8441CBE2DEFCCF1958378&searchQuery=hotel&category=hotels&latLong=-23.537430585909647%252C%2520-46.51669981349255&language=pt-br";
-    const options = { 
-        method: 'GET', 
+    const options = {
+        method: 'GET',
         headers: { accept: 'application/json' },
-        mode: 'no-cors', 
+        mode: 'no-cors',
     };
 
-    const response = await fetch(url, options)
-    console.log(1213, response.json())
+    try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+            throw new Error('Erro na solicitação.');
+        }
+        const data = await response.json();
+        console.log('Resposta do servidor: ', data);
+    } catch (error) {
+        console.error('Erro na solicitação: ', error);
+    }
 }
