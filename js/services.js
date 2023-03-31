3e0c495997cd0e63e2a8a8657d19a80c1a8ed57d
elio.lima <macstudio@local>
2023-03-31 19:35:21 -0300

    feat(Fix): ajust coments

--- js/services.js
+++ js/services.js
@@ -1,18 +1,16 @@
 
-$.fn.searchData = async function () {
+$.fn.searchData = function () {
     const url = "https://p7167519uh.execute-api.us-east-1.amazonaws.com/dev/search?searchQuery=hotel&category=hotels&latLong=-23.537430585909647%252C%2520-46.51669981349255&language=pt-br";
-    fetch(url, {    
-      method: 'GET',    
-      withCredentials: true,    
-      crossorigin: true,    
-      mode: 'no-cors',       
-    })    
-      .then((res) => res.json())    
-      .then((data) => {    
-        console.log(data);    
-      })    
-      .catch((error) => {    
-        console.error(error);    
-      });    
-  };    
+    fetch(url, {
+        method: 'GET',
+        withCredentials: true,
+        crossorigin: true,
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
 }
