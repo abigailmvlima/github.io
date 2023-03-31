da8c3c6984a5dd5785ece2b9f9be6e3b545c6f4e
elio.lima <macstudio@local>
2023-03-31 14:21:41 -0300

    feat(Fix): ajust coments

--- js/services.js
+++ js/services.js
@@ -1,7 +1,11 @@
 
 $.fn.searchData = function () {
     const url = "https://api.content.tripadvisor.com/api/v1/location/search?key=A0A244D2D5D8441CBE2DEFCCF1958378&searchQuery=hotel&category=hotels&latLong=-23.537430585909647%252C%2520-46.51669981349255&language=pt-br";
-    const options = { method: 'GET', headers: { accept: 'application/json' } };
+    const options = { 
+        method: 'GET', 
+        headers: { accept: 'application/json' },
+        mode: 'no-cors', 
+    };
 
     fetch(url, options)
         .then(response => console.log(1111,response.json()) )
