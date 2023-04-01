7eee5d9ade5802a979d52028f68cdc5ed90b6cdb
elio.lima <macstudio@local>
2023-03-31 21:22:46 -0300

    feat(Fix): ajust coments

--- js/load.js
+++ js/load.js
@@ -2,7 +2,5 @@ $( document ).ready(function() {
     var today = moment().format('YYYY-MM-DD');
     document.getElementById("dateStart").value = today;
     document.getElementById("dateEnd").value = today;
-
-
     $.fn.loadListaCards()
 });
--- js/services.js
+++ js/services.js
@@ -36,26 +36,30 @@ $.fn.editCard = function ({ texto, description, img, imgRate }) {
 }
 
 $.fn.loadListaCards = async function () {
-    var html = ''
-    var pesquisa = '?searchQuery=hotel&category=hotels&latLong=-23.537430585909647%252C%2520-46.51669981349255&language=pt-br'
-    var list = await $.fn.request('search',pesquisa)
-    var data = list.data
+    try {
+        var html = ''
+        var pesquisa = '?searchQuery=hotel&category=hotels&latLong=-23.537430585909647%252C%2520-46.51669981349255&language=pt-br'
+        var list = await $.fn.request('search', pesquisa)
+        var data = list.data
 
-    // percorre os 3 primeiros registros
-    for (let i = 0; i < data.length && i < 3; i++) {
-        const registro = data[i];
-        var photos = await $.fn.request('search/photos',`?location_id=${registro.location_id}`)
-        var reviews = await $.fn.request('location/reviews',`?location_id=${registro.location_id}`)
-        var texto = reviews.data[0].text.substring(0, 200)
+        // percorre os 3 primeiros registros
+        for (let i = 0; i < data.length && i < 3; i++) {
+            const registro = data[i];
+            var photos = await $.fn.request('search/photos', `?location_id=${registro.location_id}`)
+            var reviews = await $.fn.request('location/reviews', `?location_id=${registro.location_id}`)
+            var texto = reviews.data[0].text.substring(0, 200)
 
-        // cria o card para home
-        html += $.fn.editCard({
-            texto: `${texto}...`,
-            imgRate: reviews.data[0].rating_image_url,
-            description: registro.name,
-            img: photos.data[0].images.original,
-        }) 
+            // cria o card para home
+            html += $.fn.editCard({
+                texto: `${texto}...`,
+                imgRate: reviews.data[0].rating_image_url,
+                description: registro.name,
+                img: photos.data[0].images.original,
+            })
+        }
+
+        $('#listaCards').html(html)
+    } catch (error) {
+        console.error(error)
     }
-
-    $('#listaCards').html(html)
 }
