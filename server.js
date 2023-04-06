03327023c578608e451e12c48a952ee1b4ea7194
Abigail Lima <abigailmvlima@outlool.com>
2023-04-06 17:29:52 -0300

    porta

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
-
-app.listen(5000, () => {
-  console.log("Server started on port 3000");
+const host = "0.0.0.0";
+app.listen(5000, host, () => {
+  console.log("Server started on port 5000");
 });
