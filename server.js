f35eb75b89351acfa627bfda821323aedb952259
Abigail Lima <abigailmvlima@outlool.com>
2023-04-06 17:54:13 -0300

    teste

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 
-// app.use(express.static("public"));
+const port = process.env.PORT || 3000;
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  // res.sendFile(__dirname + "/index.html");
-  res.status(200).json({});
+  res.sendFile(__dirname + "/index.html");
 });
-const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log("Server started on port 5000");
 });
