e136fb3e9221e29de9868b37bbfa37076b0cd89a
Kaique Bernardo <kaique@MacBook-Kaique.local>
2023-04-06 20:33:13 -0300

    routes fix

---
+++ public/views/details/js/load.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+    var today = moment().format('YYYY-MM-DD');
+    document.getElementById("dateStart").value = today;
+    document.getElementById("dateEnd").value = today;
+    $.fn.loadListaCards()
+});
