ea016ff08ee4e81b983c0a4a0fd09d46cf2e98f6
Abigail Lima <abigailmvlima@outlool.com>
2023-04-06 17:21:44 -0300

    alterei a porta para 5000

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(5000, () => {
+  console.log("Server started on port 3000");
 });
