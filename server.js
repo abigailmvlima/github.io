7799b17897a23fc75099ddcfdac6db0d6dd33171
Abigail Lima <abigailmvlima@outlool.com>
2023-04-06 17:40:26 -0300

    teste

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const app = express();
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  // res.sendFile(__dirname + "/index.html");
+  res.sendFile("/index.html");
 });
 const host = "0.0.0.0";
 app.listen(5000, host, () => {
