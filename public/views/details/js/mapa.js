d96c549c018a5edbeff7e76ef106153474a87e05
Kaique Bernardo <kaique@MacBook-Kaique.local>
2023-04-07 20:03:41 -0300

    pendente views

--- public/views/details/js/mapa.js
+++ public/views/details/js/mapa.js
@@ -1,5 +1,6 @@
 function carregarMapa() {
   obterLocalizacao();
+  console.log(9999, "dale");
 }
 
 function obterLocalizacao() {
@@ -18,12 +19,24 @@ function obterPosicao(position) {
 }
 
 function mapa(latitude, longitude) {
+  var markerSize = { sm: [28, 35], md: [35, 44], lg: [42, 53] };
+  var markerAnchor = { sm: [14, 35], md: [17, 44], lg: [21, 53] };
+  var markerPopupAnchor = { sm: [1, -35], md: [1, -44], lg: [2, -53] };
+
   var url =
     "https://www.mapquestapi.com/staticmap/v5/map?key=kGfI7mKWhWfLAC1fzS8djXwadtyGjqgt&center=" +
     latitude +
     "," +
     longitude +
-    "&size=400,400@2x";
+    "&size=850,400@2x";
+
+  var mediumMarker = L.icon({
+    iconUrl: "https://assets.mapquestapi.com/icon/v2/marker-md.png",
+    iconRetinaUrl: "https://assets.mapquestapi.com/icon/v2/marker-md@2x.png",
+    iconSize: markerSize.md,
+    iconAnchor: markerAnchor.md,
+    popupAnchor: markerPopupAnchor.md,
+  });
 
   var image = document.createElement("img");
   image.src = url;
